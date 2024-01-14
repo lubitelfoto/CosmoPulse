1878ab6f38fe4b9bd58c66a296589370d34ef6c2
Pavel <hydrophonyx@gmail.com>
2024-01-15 00:16:57 +0300

    rename app to main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from app.model_manager import ModelManager
+from main.model_manager import ModelManager
 from fastapi import FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
